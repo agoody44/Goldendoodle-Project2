68d065d40fc760c2eb704cf51faaa809fba0c4f5
“Alyssa <agoody44@gmail.com>
2020-11-17 15:49:40 -0500

    updates playground

--- playground/bcrypt.js
+++ playground/bcrypt.js
@@ -6,6 +6,6 @@ const SALT_ROUNDS = bcrypt.genSaltSync(10);
 
 const password = 'supersecretpassword';
 
-const hashedString = bcrypt.hashSync(password, SALT_ROUNDS);
+// const hashedString = bcrypt.hashSync(password, SALT_ROUNDS);
 
-const isMatch = bcrypt.compareSync('passwordProvidedAtLogin', hashedPassword);
+// const isMatch = bcrypt.compareSync('passwordProvidedAtLogin', hashedPassword);
